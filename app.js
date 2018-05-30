854103e92730caf4db44eba823b6344a57c42eff
Indimeco <18373729+Indimeco@users.noreply.github.com>
2018-05-30 13:16:22 +1000

    Added custom scroll spy to the nav Updated smooth scroll with new nav classes

--- app.js
+++ app.js
@@ -6,30 +6,52 @@
 		rotateText('rotator',['simplicity', 'communication', 'expandability', 'utility', 'sustainability'], 0);
 		// Smooth scrolling with jQuery
 		$('a.js-scroll-trigger[href*="#"]').click(smoothScroll);
+		// Scroll spy
+		window.onscroll = scrollSpy;
 	});
 
-	function rotateText(target,textArr,index){
+	function rotateText(target,textArr,index){ // power the changing text in the about section
 		$('#'+target).html(textArr[index]).fadeIn(1600, function() {
-			index==textArr.length-1 ? index=0 : index++; // loop index	
+			index==textArr.length-1 ? index=0 : index++; // loop index infinitely 
 			$('#'+target).fadeOut(1600, function() {					
-				rotateText(target,textArr,index); // recursively continue animating					
+				rotateText(target,textArr,index); // recursively continue animating			
 			}); 
 		});
 	}
 
-	function smoothScroll(event) {
+	function smoothScroll(event) { // animate scrolling transition to clicked section
 		event.preventDefault();
 		var location = $(this.hash); // location id e.g., '#portfolio'
 		if (location.length) {
-			$('a.nav-link:not([href*="#'+location[0].id+'"])').removeClass('animated-underline-ribbon'); // prevent other nav items from animating
-			$('a.nav-link[href*="#'+location[0].id+'"]').addClass('force-underlined'); // ensure the clicked nav item remains animated during scroll
+			$('nav div a:not([href*="#'+location[0].id+'"])').removeClass('animated-underline-ribbon'); // prevent other nav items from animating
+			$('nav div a[href*="#'+location[0].id+'"]').addClass('force-underlined'); // ensure the clicked nav item remains animated during scroll
 			$('html, body').animate({
 				scrollTop: (location.offset().top - 20) // additional space above location
 			}, 800);
 			setTimeout(function(loc) { // restore nav links to default after scrolling is complete
-				$('a.nav-link:not([href*="#'+loc+'"])').addClass('animated-underline-ribbon');
-				$('a.nav-link[href*="#'+loc+'"]').removeClass('force-underlined');
+				$('nav div a:not([href*="#'+loc+'"])').addClass('animated-underline-ribbon');
+				$('nav div a[href*="#'+loc+'"]').removeClass('force-underlined');
 			},800,location[0].id);
 		}
 	}
+	
+	function scrollSpy() { // update navbar to match current scrolled content
+		var sections = $('section'); // array of all sections
+		var locations = {};
+		for (var i=0; i<sections.length; i++) {
+			locations[sections[i].id] = sections[i].offsetTop - 100; // object containing scroll locations keyed by section id
+		}
+		
+		var scrollPosition = $(document).scrollTop(); // current scroll location 
+		var target = 'about'; // default behavior if scrolling in non section areas
+		for (var i in locations) {
+			if (locations[i] <= scrollPosition) {
+				target = i || 'about';
+			}
+		}
+		
+		$('.active').removeClass('active'); // remove underline from old navbar section representation
+		$('#nav-'+target).addClass('active'); // add it to the current navbar section representation
+	}
+	
 }());
